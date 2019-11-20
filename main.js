11d781311a3864c6b7417d3e9e95fa7f7b8ea91d
hectorcarrasco1992 <hector.carrasco@codeimmersives.com>
2019-11-20 18:18:51 -0500

    tried changing canvas

--- main.js
+++ main.js
@@ -9,8 +9,47 @@
 
     document.querySelector('.color-5').classList.add('green')
 
+    const brush = document.querySelector('.current-brush')
+    
+    // event listeners//
+    document.querySelector('.color-1').addEventListener('click',addRed)
+    document.querySelector('.color-2').addEventListener('click',addBlue)
+    document.querySelector('.color-3').addEventListener('click',addPink)
+    document.querySelector('.color-4').addEventListener('click',addPurple)
+    document.querySelector('.color-5').addEventListener('click',addGreen)
+    document.querySelector('.square').addEventListener('click',paint)
+    
     function addRed(){
-        document.querySelector('.brush').classList.toggle('red')
+        // document.querySelector('.current-brush').classList.remove('')
+        
+        document.querySelector('.current-brush').classList.toggle('red')
     }
 
-    document.querySelector('.color-1').addEventListener('click',addRed)
+    function addBlue(){
+        
+        document.querySelector('.current-brush').classList.toggle('blue')
+
+    }
+    function addPink(){
+        
+        document.querySelector('.current-brush').classList.toggle('pink')
+
+    }
+    function addPurple(){
+        document.querySelector('.current-brush').classList.toggle('purple')
+
+    }
+    function addGreen(){
+        document.querySelector('.current-brush').classList.toggle('green')
+
+    }
+
+    function paint(){
+        if(brush.classList.contains('red')){
+            document.querySelector('.square').classList.add('red')
+        }
+            while(brush.classList.contains(blue)){
+                document.querySelector('.square').classList.add('blue')
+            }
+        
+    }
