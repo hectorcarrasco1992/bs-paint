8ddf38404a3eddb3b80f0c40fbb0a9f0a465ba5a
hectorcarrasco1992 <hector.carrasco@codeimmersives.com>
2019-11-20 15:47:19 -0500

    changed palette colors nd added classes

---
+++ main.js
@@ -0,0 +1,10 @@
+
+    document.querySelector('.color-1').classList.add('red')
+
+    document.querySelector('.color-2').classList.add('blue')
+
+    document.querySelector('.color-3').classList.add('pink')
+
+    document.querySelector('.color-4').classList.add('purple')
+
+    document.querySelector('.color-5').classList.add('green')
