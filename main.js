59ae9c96a77d45135bc1cac1c6ee2f34fe99c079
hectorcarrasco1992 <hector.carrasco@codeimmersives.com>
2019-11-20 20:17:31 -0500

    attempted to fix functins for colors

--- main.js
+++ main.js
@@ -9,47 +9,46 @@
 
     document.querySelector('.color-5').classList.add('green')
 
-    const brush = document.querySelector('.current-brush')
+    
     
     // event listeners//
-    document.querySelector('.color-1').addEventListener('click',addRed)
-    document.querySelector('.color-2').addEventListener('click',addBlue)
-    document.querySelector('.color-3').addEventListener('click',addPink)
-    document.querySelector('.color-4').addEventListener('click',addPurple)
-    document.querySelector('.color-5').addEventListener('click',addGreen)
-    document.querySelector('.square').addEventListener('click',paint)
+    document.querySelector('.color-1').addEventListener('click', addBlue('.current-brush'))
+    // document.querySelector('.color-2').addEventListener('click',addBlue('.current-brush'))
+    // document.querySelector('.color-3').addEventListener('click',addPink)
+    // document.querySelector('.color-4').addEventListener('click',addPurple)
+    // document.querySelector('.color-5').addEventListener('click',addGreen)
+    // document.querySelector('.square').addEventListener('click',paint)
     
-    function addRed(){
+    function addRed(classNamez){
         // document.querySelector('.current-brush').classList.remove('')
         
-        document.querySelector('.current-brush').classList.toggle('red')
+        document.querySelector(classNamez).classList.toggle('red')
     }
 
-    function addBlue(){
+    function addBlue(classNamez){
         
-        document.querySelector('.current-brush').classList.toggle('blue')
+        document.querySelector(classNamez).classList.toggle('blue')
 
     }
-    function addPink(){
+    function addPink(className){
         
-        document.querySelector('.current-brush').classList.toggle('pink')
+        document.querySelector(className).classList.toggle('pink')
 
     }
-    function addPurple(){
-        document.querySelector('.current-brush').classList.toggle('purple')
+    function addPurple(className){
+        document.querySelector(className).classList.toggle('purple')
 
     }
-    function addGreen(){
-        document.querySelector('.current-brush').classList.toggle('green')
+    function addGreen(className){
+        document.querySelector(className).classList.toggle('green')
 
     }
 
     function paint(){
-        if(brush.classList.contains('red')){
-            document.querySelector('.square').classList.add('red')
-        }
-            while(brush.classList.contains(blue)){
-                document.querySelector('.square').classList.add('blue')
-            }
         
+        
+        
+        
+            
     }
+    
