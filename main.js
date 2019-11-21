67d1572795fcf3cdf0f7c837d884c21b05949286
hectorcarrasco1992 <hector.carrasco@codeimmersives.com>
2019-11-21 11:34:38 -0500

    added event listeners and fixed code

--- main.js
+++ main.js
@@ -1,56 +1,80 @@
 
-    document.querySelector('.color-1').classList.add('red')
+    document.querySelector('.color-red').classList.add('red')
 
-    document.querySelector('.color-2').classList.add('blue')
+    document.querySelector('.color-blue').classList.add('blue')
 
-    document.querySelector('.color-3').classList.add('pink')
+    document.querySelector('.color-pink').classList.add('pink')
 
-    document.querySelector('.color-4').classList.add('purple')
+    document.querySelector('.color-purple').classList.add('purple')
 
-    document.querySelector('.color-5').classList.add('green')
+    document.querySelector('.color-green').classList.add('green')
 
     
     
     // event listeners//
-    document.querySelector('.color-1').addEventListener('click', addRed('.current-brush'))
-    document.querySelector('.color-2').addEventListener('click',addBlue('.current-brush'))
-    document.querySelector('.color-3').addEventListener('click',addPink('.current-brush'))
-    document.querySelector('.color-4').addEventListener('click',addPurple('.current-brush'))
-    document.querySelector('.color-5').addEventListener('click',addGreen('.current-brush'))
-    document.querySelector('.square').addEventListener('click',paint)
+    document.querySelector('.color-red').addEventListener('click', addColor)
+    document.querySelector('.color-blue').addEventListener('click',addColor)
+    document.querySelector('.color-pink').addEventListener('click',addColor)
+    document.querySelector('.color-purple').addEventListener('click',addColor)
+    document.querySelector('.color-green').addEventListener('click',addColor)
+    // document.querySelector('.square').addEventListener('click',paint)
     
-    function addRed(classNameZ){
-        // document.querySelector('.current-brush').classList.remove('')
-        if(document.querySelector(classNameZ).classList.contains('.red')===false){
-
-            document.querySelector(classNameZ).classList.add('red')
-        }
-    }
-
-    function addBlue(classNameZ){
+    // One function that:
+    // * Checks what palette square was clicked.
+    // * Checks the color class of that palette square.
+    // * Adds that color class to the current brush.
+    const currentBrush = document.querySelector('.current-brush')
+    function addColor(event){
+        document.querySelector('.current-brush').classList.replace(currentBrush.classList[1], event.target.classList[2])
+        // event.target.classList.add('red')
         
-        document.querySelector(classNameZ).classList.toggle('blue')
-
-    }
-    function addPink(classNameZ){
         
-        document.querySelector(classNameZ).classList.toggle('pink')
-
     }
-    function addPurple(classNameZ){
-        document.querySelector(classNameZ).classList.toggle('purple')
 
-    }
-    function addGreen(classNameZ){
-        document.querySelector(classNameZ).classList.toggle('green')
+    const canvasSquare = document.querySelectorAll('.square')
+
+    for (let i =0;i<canvasSquare.length;i++){
+        canvasSquare[i].addEventListener('click',changeSquare)
 
     }
 
-    function paint(){
+    function changeSquare(event){
+        event.target.classList.replace(event.target.classList[1],currentBrush.classList[1])
+    }
+    
+
+    // function addBlue(){
+        
+    //     document.querySelector('.current-brush').classList.toggle('blue')
+
+    // }
+    // function addPink(){
+        
+    //     document.querySelector('.current-brush').classList.toggle('pink')
+
+    // }
+    // function addPurple(){
+    //     document.querySelector('.current-brush').classList.toggle('purple')
+
+    // }
+    // function addGreen(){
+    //     document.querySelector('.current-brush').classList.toggle('green')
+
+    //}
+
+    // function paint(event){
+    //     const brush = document.querySelector('.current-brush')
+    //     const canvasSquare = document.querySelectorAll('.square')
+
+    //     for (let i = 0)
+        
+        
+
+        
         
         
         
         
             
-    }
+    // }
     
